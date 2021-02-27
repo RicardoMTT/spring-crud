317e77e24302d69490d20082464efa3ea925c743
Ricardo Tovar <ricardotovar.grupobruild@gmail.com>
2021-02-27 14:19:18 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'springboot-api-rest';
+  constructor(private httpClient: HttpClient) {
+    this.httpClient.get('http://localhost:8080/personas').subscribe((data) => {
+      console.log('dataaa', data);
+    });
+  }
 }
